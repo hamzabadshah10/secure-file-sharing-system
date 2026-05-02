e336c7146582e4e5807eafa139d10700fef8a0f3
Aman Sajid <amansajid078@gmail.com>
2026-05-02 23:07:35 +0500

    Create UserRepository.java file

---
+++ backend/src/main/java/com/paf/securefile/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.paf.securefile.repository;
+
+import com.paf.securefile.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
